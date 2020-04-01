a32fbac473488673648acc76815908b64aca7ba5
Sandro E Martinez <zanddro@gmail.com>
2020-03-31 22:02:52 -0600

    web support code added by default in thome template

--- src/Templates/home/index.js
+++ src/Templates/home/index.js
@@ -1,3 +1,11 @@
 import PageTemplate from '@codeinc.mx/page-template-component/js/PageTemplate';
+import WebpSupportPlugin from '@codeinc.mx/background-manager/js/plugins/WebpSupportPlugin.js'
 
-new PageTemplate( { document });
+new PageTemplate({
+  document,
+  plugins: [
+    new WebpSupportPlugin({
+      noWebpSupportedClassName: 'webp--disabled'
+    })
+  ]
+});
